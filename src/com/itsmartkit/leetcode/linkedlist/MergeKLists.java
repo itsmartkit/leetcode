dc5d96fad06264f905895ee4ec0d6d2f46a0d08f
geekeep <changyj91@gmail.com>
2020-05-28 01:12:05 +0800

    合并K个排序链表

---
+++ src/com/itsmartkit/leetcode/linkedlist/MergeKLists.java
@@ -0,0 +1,72 @@
+package com.itsmartkit.leetcode.linkedlist;
+
+import com.itsmartkit.leetcode.linkedlist.common.ListNode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @ClassName MergeKLists
+ * @Description TODO 合并K个排序链表
+ * @Author changyj
+ * @Date 2020-05-27 23:41
+ */
+public class MergeKLists {
+
+
+    /**
+     * 优先级队列
+     */
+    public ListNode mergeKLists1(ListNode[] lists) {
+        Queue<ListNode> pq = new PriorityQueue<>((v1, v2) -> v1.val - v2.val);
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            tail.next = minNode;
+            tail = minNode;
+            if (minNode.next != null) {
+                pq.offer(minNode.next);
+            }
+        }
+        return dummyHead.next;
+    }
+
+
+    /**
+     * 分而治之，递归
+     */
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+
+
+}
