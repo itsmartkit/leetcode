cd42303ce72097da9a8b6eb39e44ccf53ca7113e
changyj <changyj@huak.com.cn>
2020-05-28 16:25:11 +0800

    栈排序/最小栈/最大栈

--- src/com/itsmartkit/leetcode/SortedStack.java
+++ src/com/itsmartkit/leetcode/SortedStack.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 /**
  * @author cyj
  * @name SortedStack
- * @description TODO 栈排序/最小栈
+ * @description TODO 栈排序/最小栈/最大栈
  * @date 2020/5/28 13:49
  * Version 1.0
  */
@@ -18,23 +18,23 @@ public class SortedStack {
      */
 
     private Stack<Integer> stack;
-    private Stack<Integer> temp;
+    private Stack<Integer> maxStack;
 
     public SortedStack() {
         stack = new Stack<>();
-        temp = new Stack<>();
+        maxStack = new Stack<>();
     }
 
     public void push(int val) {
         int max = stack.isEmpty() ? Integer.MAX_VALUE : stack.peek();
-        int min = temp.isEmpty() ? Integer.MIN_VALUE : temp.peek();
+        int min = maxStack.isEmpty() ? Integer.MIN_VALUE : maxStack.peek();
         while (true) {
             if (val > max) {
-                temp.push(stack.pop());
+                maxStack.push(stack.pop());
                 max = stack.isEmpty() ? Integer.MAX_VALUE : stack.peek();
             } else if (val < min) {
-                stack.push(temp.pop());
-                min = temp.isEmpty() ? Integer.MIN_VALUE : temp.peek();
+                stack.push(maxStack.pop());
+                min = maxStack.isEmpty() ? Integer.MIN_VALUE : maxStack.peek();
             } else {
                 stack.push(val);
                 break;
@@ -42,23 +42,66 @@ public class SortedStack {
         }
     }
 
+    /**
+     * @description //TODO 最小栈 pop()
+     * @author cyj
+     * @date 2020/5/28 16:22
+     * @return
+     * change notes
+     */
     public void pop() {
-        while (!temp.isEmpty()) {
-            stack.push(temp.pop());
+        while (!maxStack.isEmpty()) {
+            stack.push(maxStack.pop());
         }
         if (!stack.isEmpty()) {
             stack.pop();
         }
     }
-
+    /**
+     * @description //TODO 最小栈 peek()
+     * @author cyj
+     * @date 2020/5/28 16:22
+     * @return
+     * change notes
+     */
     public int peek() {
-        while (!temp.isEmpty()) {
-            stack.push(temp.pop());
+        while (!maxStack.isEmpty()) {
+            stack.push(maxStack.pop());
         }
         return stack.isEmpty() ? -1 : stack.peek();
     }
 
+
+    /**
+     * @description //TODO 最大栈 pop()
+     * @author cyj
+     * @date 2020/5/28 16:22
+     * @return
+     * change notes
+     */
+    public void popMax() {
+        while (!stack.isEmpty()) {
+            maxStack.push(stack.pop());
+        }
+        if (!maxStack.isEmpty()) {
+            maxStack.pop();
+        }
+    }
+    /**
+     * @description //TODO 最大栈 peek()
+     * @author cyj
+     * @date 2020/5/28 16:22
+     * @return
+     * change notes
+     */
+    public int peekMax() {
+        while (!stack.isEmpty()) {
+            maxStack.push(stack.pop());
+        }
+        return maxStack.isEmpty() ? -1 : maxStack.peek();
+    }
+
     public boolean isEmpty() {
-        return stack.isEmpty() && temp.isEmpty();
+        return stack.isEmpty() && maxStack.isEmpty();
     }
 }
